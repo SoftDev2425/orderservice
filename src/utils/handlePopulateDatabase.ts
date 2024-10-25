8a109071bfdf29a48cf184d3c3ec7e8ebc40f0f8
Owais Dashti <owaisdashti@outlook.com>
2024-10-25 17:35:27 +0200

    fix: ...

--- src/utils/handlePopulateDatabase.ts
+++ src/utils/handlePopulateDatabase.ts
@@ -1,23 +1,24 @@
 import prisma from '../../prisma/client';
 
 export const handlePopulateDatabase = async () => {
-  // remove from users and from TestTable and add
+  const usersData = [
+    {
+      name: 'Andreas',
+      email: 'af@af.dk',
+      password: 'password',
+    },
+    {
+      name: 'Owais',
+      email: 'ow@ow.dk',
+      password: 'password',
+    },
+  ];
 
-  await prisma.user.deleteMany();
-  await prisma.testTable.deleteMany();
-
-  await prisma.user.createMany({
-    data: [
-      {
-        name: 'Andreas',
-        email: 'af@af.dk',
-        password: 'password',
-      },
-      {
-        name: 'Owais',
-        email: 'ow@ow.dk',
-        password: 'password',
-      },
-    ],
-  });
+  for (const userData of usersData) {
+    await prisma.user.upsert({
+      where: { email: userData.email }, // Match on email to check existence
+      update: {}, // If a match is found, do nothing
+      create: userData, // If no match is found, create the new entry
+    });
+  }
 };
